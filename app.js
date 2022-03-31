9318d384a3f9040576f8a7e9791b9d6a59c1a143
Corey Morrison <coreymorrison@Coreys-MBP.home>
2022-03-30 22:17:32 -0400

    manifest added and conencted

---
+++ app.js
@@ -0,0 +1,18 @@
+//look up instances of specified text
+//use DOM manipulation to change the text to something else
+//at some point rick roll
+
+
+console.log('App.js is working!')
+
+document.body.innerHTML = document.body.innerHTML.replaceAll(/log in/g, 'hi');
+
+// let selectP = document.querySelectorAll('p')
+// let selectP = document.getElementsByTagName("p").innerHTML
+// console.log(selectP)
+// selectP.forEach(el => {
+//     console.log(el)
+//   })
+
+
+
